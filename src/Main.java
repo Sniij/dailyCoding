c5ac5a95d73c8709675775a853c7d21e78ef84ee
Sniij <dokeycho@gmail.com>
2023-06-14 23:08:48 +0900

    add 178871

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import programmers.level1.A178871;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        A178871 a178871 = new A178871();
+        a178871.main();
     }
 }
