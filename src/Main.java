b7f96aeaa064aa0bd03edfb9ed463d4da08a0c00
Sniij <dokeycho@gmail.com>
2023-06-15 21:16:42 +0900

    pass 176963

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import programmers.level1.pass.P176963;
 import programmers.level1.pass.P178871;
 
 public class Main {
@@ -5,5 +6,8 @@ public class Main {
 
         P178871 p178871 = new P178871();
         p178871.test();
+
+        P176963 p176963 = new P176963();
+        p176963.test();
     }
 }
