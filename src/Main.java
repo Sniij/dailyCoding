525a627ba6821e923a58f0849d18a74fe74b4a7a
Sniij <dokeycho@gmail.com>
2023-06-16 21:57:45 +0900

    pass 176963

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,16 @@ import programmers.level1.pass.P178871;
 public class Main {
     public static void main(String[] args) {
 
-        P178871 p178871 = new P178871();
-        p178871.test();
 
+        // pass 목록
+
+        P178871 p178871 = new P178871();
+        //p178871.test();
         P176963 p176963 = new P176963();
-        p176963.test();
+        //p176963.test();
+
+
+
+
     }
 }
---
+++ src/programmers/level1/fail/F172928.java
@@ -0,0 +1,24 @@
+package programmers.level1.fail;
+
+import java.util.Arrays;
+
+public class F172928 {
+
+
+
+    public void test(){
+
+        String[] park = {"SOO","OOO","OOO"};
+        String[] routes = {"E 2","S 2","W 1"};
+
+
+        System.out.println(Arrays.toString(solution(park, routes)));
+    }
+
+
+    public int[] solution(String[] park, String[] routes) {
+
+        int[] answer = {};
+        return answer;
+    }
+}
