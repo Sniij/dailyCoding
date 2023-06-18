923fe837c545bf28444dcfc55bd4b699267adb21
Sniij <dokeycho@gmail.com>
2023-06-18 15:13:55 +0900

    pass 161990

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import programmers.level1.pass.P161990;
 import programmers.level1.pass.P172928;
 import programmers.level1.pass.P176963;
 import programmers.level1.pass.P178871;
@@ -12,10 +13,10 @@ public class Main {
         //p178871.test();
         P176963 p176963 = new P176963();
         //p176963.test();
-
         P172928 p172928 = new P172928();
-        p172928.test();
-
+        //p172928.test();
+        P161990 p161990 = new P161990();
+       // p161990.test();
 
 
     }
