abdfd8a8c8086e98504667a365173b0256edede6
Sniij <120099321+Sniij@users.noreply.github.com>
2023-08-17 02:48:15 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.12 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        String[] strings = s.split(" ");
+        for(int i =0; i<strings.length; i++){
+        //System.out.println(strings[i]);
+
+            if(strings[i].equals("Z")){
+                answer -= Integer.parseInt(strings[i-1]);
+            }else{
+                answer += Integer.parseInt(strings[i]);
+            }
+
+        }
+        
+        
+        return answer;
+    }
+}
