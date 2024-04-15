5602922904075c4c94f46437c47901b7acc01652
Sniij <120099321+Sniij@users.noreply.github.com>
2024-04-15 23:55:40 +0900

    pass 73529

---
+++ src/programmers/level2/P73529.java
@@ -0,0 +1,27 @@
+package programmers.level2;
+
+import java.util.*;
+
+public class P73529 {
+
+    class Solution {
+        public int solution(int[] peoples, int limit) {
+            List<Integer> list = new ArrayList<>();
+            for(int x : peoples) list.add(x);
+            Collections.sort(list);
+
+            // 꼭 사이즈 지정 필요
+            ArrayDeque<Integer> dq = new ArrayDeque<>(50505);
+            for(int x : list) dq.add(x);
+
+            int ans = 0;
+            while(dq.isEmpty() == false) {
+                int weight = dq.pollLast();
+                if(dq.isEmpty() == false && weight + dq.peekFirst() <= limit) { dq.pollFirst(); }
+                ans++;
+            }
+
+            return ans;
+        }
+    }
+}
